87c8f703327027ccd2b0bf55bee5093a145ed82e
lnmcc <lnmcc@hotmail.com>
2016-06-01 21:34:48 +0800

    init

---
+++ gvr-ios-sdk/Samples/Stars/Pods/Target Support Files/Pods-Stars/Pods-Stars-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stars : NSObject
+@end
+@implementation PodsDummy_Pods_Stars
+@end
---
+++ gvr-ios-sdk/Samples/TreasureHunt/Pods/Target Support Files/Pods-TreasureHunt/Pods-TreasureHunt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TreasureHunt : NSObject
+@end
+@implementation PodsDummy_Pods_TreasureHunt
+@end
